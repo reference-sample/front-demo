43c414970cd9dddd1b9b762ac4eb2579629f94be
shiyuan <wangyuanhpu@163.com>
2025-07-30 22:07:11 +0800

    索引页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,40 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
+export const routes = [
+  {
+    path: '/',
+    name: 'index',
+    component: () => import('../views/IndexView.vue'),
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: () => import('../views/HomeView.vue'),
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/LoginView.vue'),
+  },
+  {
+    path: '/file-upload',
+    name: 'file-upload',
+    component: () => import('../views/FileUploadView.vue'),
+  },
+  {
+    path: '/large-file-upload',
+    name: 'large-file-upload',
+    component: () => import('../views/LargeFileUploadView.vue'),
+  },
+  {
+    path: '/large-file-download',
+    name: 'large-file-download',
+    component: () => import('../views/LargeFileDownloadView.vue'),
+  },
+]
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: () => import('../views/LoginView.vue'),
-    },
-    {
-      path: '/file-upload',
-      name: 'file-upload',
-      component: () => import('../views/FileUploadView.vue'),
-    },
-    {
-      path: '/large-file-upload',
-      name: 'large-file-upload',
-      component: () => import('../views/LargeFileUploadView.vue'),
-    },
-    {
-      path: '/large-file-download',
-      name: 'large-file-download',
-      component: () => import('../views/LargeFileDownloadView.vue'),
-    }
-  ],
+  routes,
 })
 
 export default router
---
+++ src/views/IndexView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="index">
+    <RouterLink :to="route.path" v-for="(route, index) in routes" :key="route.path">{{ route.name }}</RouterLink>
+  </div>
+</template>
+<script setup>
+import { RouterLink } from 'vue-router'
+import {routes} from '../router/index'
+console.info(routes);
+</script>
+<style lang="css" scoped>
+.index a {
+    display: inline-block;
+    margin: 20px;
+  }
+</style>
